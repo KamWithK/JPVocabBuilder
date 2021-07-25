97e2e6b024e4041601798a445bed7a1fe7927c8a
Kamron Bhavnagri <kamwithk@tuta.io>
2021-07-26 01:59:40 +1000

    Dropzone element

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -1,44 +1,33 @@
 <script lang="ts">
-	import rust from "../../Cargo.toml";
+	import rust from '../../Cargo.toml';
+	import Dropzone from './Dropzone.svelte';
 
-	async function onFilesSelected(e) {
-		let files = e.target.files;
+	async function onFilesSelected(event) {
 		const wasm = await rust();
-
-		for (var index = 0; index < files.length; index++) {
-			let file = files[index];
-			const extention = file.name.substring(file.name.lastIndexOf('.') + 1);
-
-			let reader = new FileReader();
-			reader.readAsText(file);
-			reader.onload = e => wasm.parse_subtitle(e.target.result, extention);
-		}
+		wasm.parse_subtitle(event.detail.content, event.detail.extention);
 	}
 </script>
 
 <main>
 	<h1>Japanese Vocab Builder</h1>
-	<input type="file" id="fileInput" multiple on:change={onFilesSelected}/>
+	<Dropzone on:file_read={onFilesSelected}/>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		height: 100%;
+		padding: 0 em;
 		margin: 0 auto;
 	}
 
 	h1 {
 		color: #ff3e00;
+		text-align: center;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
---
+++ src/svelte/Dropzone.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+    import '@fortawesome/fontawesome-free/js/brands.js';
+    import '@fortawesome/fontawesome-free/js/solid.js';
+    import '@fortawesome/fontawesome-free/js/fontawesome.js';
+
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+	async function handleFiles(event) {
+        let files = event instanceof FileList ? event : event.target.files;
+
+        for (var index = 0; index < files.length; index++) {
+			let file = files[index];
+			const extention = file.name.substring(file.name.lastIndexOf('.') + 1);
+
+			let reader = new FileReader();
+			reader.readAsText(file);
+
+            reader.onload = e => dispatch('file_read', {
+                content: e.target.result,
+                extention: extention
+            });
+		}
+	}
+
+    function update(event) {
+        event.stopPropagation();
+        event.preventDefault();
+    }
+
+    function drop(event) {
+        update(event);
+
+        handleFiles(event.dataTransfer.files);
+    }
+</script>
+
+<main>
+    <input type="file" id="fileInput" multiple class="visually-hidden" on:change={handleFiles}/>
+    <label id="dropzone" for="fileInput" on:dragenter={update} on:dragover={update} on:drop={drop}>
+        <h1>Drag Your Subtitles In for Analysis</h1>
+        <i id="symbol" class="fas fa-folder-open" data-fa-transform="rotate-20"></i>
+    </label>
+</main>
+
+<style>
+    main {
+        flex-grow: 1;
+    }
+
+	.visually-hidden {
+		position: absolute !important;
+		height: 1px;
+		width: 1px;
+		overflow: hidden;
+		clip: rect(1px, 1px, 1px, 1px);
+	}
+
+	input.visually-hidden:focus + label {
+		outline: thin dotted;
+	}
+
+	input.visually-hidden:focus-within + label {
+		outline: thin dotted;
+	}
+
+    #dropzone {
+        height: 100%;
+        width: 100%;
+		text-align: center;
+    }
+
+    #symbol {
+        font-size: 50vmin;
+    }
+</style>
